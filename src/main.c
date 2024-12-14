169708279d403ef198161738072db6e60fb37b0a
andrey <varlamov.and@gmail.com>
2024-12-14 02:31:23 -0800

    Create vulkan instance

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <GLFW/glfw3.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <vulkan/vulkan.h>
+
 
 int main(int argc, char **argv) {
     if (!glfwInit()) {
@@ -16,10 +19,61 @@ int main(int argc, char **argv) {
 
     glfwMakeContextCurrent(window);
 
+    VkInstance instance;
+
+    /*
+      typedef struct VkApplicationInfo {
+      VkStructureType    sType;
+      const void*        pNext;
+      const char*        pApplicationName;
+      uint32_t           applicationVersion;
+      const char*        pEngineName;
+      uint32_t           engineVersion;
+      uint32_t           apiVersion;
+      } VkApplicationInfo;
+    */
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Explore Vulkan";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "Best Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    /*
+      typedef struct VkInstanceCreateInfo {
+      VkStructureType             sType;
+      const void*                 pNext;
+      VkInstanceCreateFlags       flags;
+      const VkApplicationInfo*    pApplicationInfo;
+      uint32_t                    enabledLayerCount;
+      const char* const*          ppEnabledLayerNames;
+      uint32_t                    enabledExtensionCount;
+      const char* const*          ppEnabledExtensionNames;
+      } VkInstanceCreateInfo;
+    */
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+        printf("glfwExtensions[%u] = %s\n", i, glfwExtensions[i]);
+    }
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
+        printf("Failed to create Vulkan instance!\n");
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
+    vkDestroyInstance(instance, NULL);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
