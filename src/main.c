0f157104252ca31797a4434ab25ea3d4b603067d
andrey <varlamov.and@gmail.com>
2024-12-14 01:52:27 -0800

    Add glfw

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
+#include <GLFW/glfw3.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Hello world!\n");
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to intialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello world", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
